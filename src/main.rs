193135268dc8e9bc0d2eebc865bc30f9e3392046
yescine <bouchoucha.yassine@gmail.com>
2024-11-06 13:21:33 +0000

    more syntax sugar

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,14 @@ fn hello (){
 fn main () {
   hello();
 
+  let (resp,..) = ("data",4,5,10); 
+  let [..,z] = [1,2,3,4,5,6];
+
   let mut x:i32;
   x = 1;
   x +=4;
   assert_eq!(x,5);
-  println!("success!");
+
+  println!("\nsuccess! {}  with array {}", resp, z);
 
 }
